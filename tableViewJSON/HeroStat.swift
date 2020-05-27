1b9304d13225c95e51bf78bb6e0a517201fce510
huynghiem99 <huynghiem99@gmail.com>
2020-05-27 10:39:02 +0700

    tableJson

---
+++ tableViewJSON/HeroStat.swift
@@ -0,0 +1,9 @@
+//
+//  HeroStat.swift
+//  tableViewJSON
+//
+//  Created by macOS on 4/24/20.
+//  Copyright © 2020 macOS. All rights reserved.
+//
+
+import Foundation
---
+++ tableViewJSON/TableViewCell1.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell1.swift
+//  tableViewJSON
+//
+//  Created by macOS on 4/24/20.
+//  Copyright © 2020 macOS. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
